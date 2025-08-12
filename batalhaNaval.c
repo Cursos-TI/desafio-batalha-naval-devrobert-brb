8c4136e55f0bceb911f06634cd4dbd95d9bfa31d
RobertBarbosa <robertkilmisterbaratheon@gmail.com>
2025-08-12 12:22:30 -0300

    Batalha naval adjustments

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -2,27 +2,49 @@
 
 int main() {
     char tabuleiro[10][10] = { {0}, {0}};
-    char navio1[3] = {3};
-    char navio2[3] = {3};
-    int posicaoNavio1 = 4;
-    int posicaoNavio2 = 6;
+    int navio1 = 3, navio2 = 3, navio3 = 3, navio4 = 3;
+    int cone = 1, cruz = 1, octa = 1;
+    int posicaoNavio1 = 4, posicaoNavio2 = 7, posicaoNavio3 = 9, posicaoNavio4 = 1;
+    int posicaoCone = 4, posicaoCruz = 6, posicaoOcta = 1;
 
+    // Navios
+    for (int k=0; k < 3; k++) {
+        tabuleiro[posicaoNavio1][k] = navio1;
+    }
+
+    for (int l=0; l < 3; l++) {
+        tabuleiro[l][posicaoNavio2] = navio2;
+    }
+
+    for (int m=0; m < 3; m++) {
+        tabuleiro[posicaoNavio3][m] = navio3;
+        posicaoNavio3 = posicaoNavio3 - 1;
+    }
+
+    for (int n=6; n > 3; n--) {
+        tabuleiro[n][posicaoNavio4] = navio4;
+        posicaoNavio4 = posicaoNavio4 + 1;
+    }
+
+    // Habilidades
+    //Cruz
+    for (int t = 0; t < 3; t++) {
+        for (int u = 2 - 0; u <= 5+ t; u++) {
+            if (t == 1) {
+                tabuleiro[t][u] = 1;
+            }else {
+                tabuleiro[t][4] = 1;
+            }
+        }
+    }
+
+    
+    //Agua
     for (int i=0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
-            if (j % 10 == 0) {
-                printf("\n");
-            }
-
-            for (int k=0; k < 3; k++) {
-                tabuleiro[posicaoNavio1][k] = navio1[0];
-            }
-
-            for (int l=0; l < 3; l++) {
-                tabuleiro[l][posicaoNavio2] = navio2[0];
-            }
-
             printf("%d ", tabuleiro[i][j]);
         }
+        printf("\n");
     }
 
     return 0;
